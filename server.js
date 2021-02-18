df9773c4fe723ff749c2bcb8f78cc2a8fff7926a
Lili Cohen <lili.cohen.tv@gmail.com>
2021-02-18 12:22:54 -0800

    adding server and models folder

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
